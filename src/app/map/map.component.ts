a3dc101a3c093da80c6935f57b09d9d66130a0f2
Chantelle Chan <chantellechan1@alumni.ubc.ca>
2020-06-22 12:43:25 -0700

    changed zoom control

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -25,7 +25,7 @@ export class MapComponent implements OnInit, AfterViewInit {
   initMap(): void {
 
     // set starting location of map
-    this.map = leaflet.map('mapDOM', { zoom: 5 });
+    this.map = leaflet.map('mapDOM', { zoom: 5, zoomControl: false });
 
     // make map tiles and add to map object
     const tiles = leaflet.tileLayer(
@@ -33,14 +33,20 @@ export class MapComponent implements OnInit, AfterViewInit {
       'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg',
       {
         maxZoom: 20,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://icons8.com/icon/sawhtsYuLGVN/brake-pad-warning">Brake Pad Warning icon by Icons8</a>'
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a> | <a href="https://icons8.com/icon/sawhtsYuLGVN/brake-pad-warning">Brake Pad Warning icon by Icons8</a>'
       }
     );
 
     tiles.addTo(this.map);
 
-    this.map.locate({ setView: true, maxZoom: 13 });
+    // add zoom control in top right
+    let zoom = leaflet.control.zoom({position: 'topright'});
+    zoom.addTo(this.map);
 
+    // set map to center on user's current location
+    this.map.locate({ setView: true, maxZoom: 10 });
+
+    // set people icons
     let personIcon = leaflet.icon({
       iconUrl: 'assets/icon.png',
       iconSize: [50, 50],
@@ -50,6 +56,9 @@ export class MapComponent implements OnInit, AfterViewInit {
     let marker = leaflet.marker([49.228468, -123.159505], {icon: personIcon});
     
     marker.addTo(this.map);
+
+    // TODO: add popup for each person
+    
 
   }
 
